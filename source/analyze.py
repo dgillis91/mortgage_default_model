6c22d68a1e2e2cd82fd77fe46b466db29fa08f20
dgillis91 <dgillis91@gmail.com>
2019-03-17 11:20:07 -0500

    refactored to use seaborn

--- source/analyze.py
+++ source/analyze.py
@@ -6,7 +6,9 @@ Created on Sat Mar 16 11:30:50 2019
 """
 
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from configfile import get_config
 from dirutil import project_directory
@@ -23,11 +25,55 @@ if __name__ == '__main__':
     performance_path = os.path.join(
         project_path, config['landing_path'], 'Performance.txt'
     )
+    analysis_path = os.path.join(project_path, 'analysis')
     
-    performance_data = pd.read_csv(performance_path, config['data_sep'])
-    foreclosure_counts = get_foreclosure_counts(performance_data)
-    
-    fc_count_axes = plt.subplot(2, 1, 1)
-    foreclosure_counts.plot(
-        kind='bar', title='foreclosure_counts', ax=fc_count_axes
+    acquisition_data = pd.read_csv(
+        os.path.join(project_path, config['diw_path'], 'diw.txt'),
+        sep=config['data_sep']        
     )
+
+    # Foreclosure frequency
+    cnt = sns.countplot(x='is_foreclosed', data=acquisition_data)
+    cnt.get_figure().savefig(os.path.join(analysis_path, 'fc_stat_freq'))
+    plt.close('all')
+    
+    # Credit Score by foreclosure status
+    credit_score_box = sns.boxenplot(
+        x='is_foreclosed', y='borrower_credit_score_at_origination',
+        data=acquisition_data
+    )
+    credit_score_box.get_figure().savefig(os.path.join(
+        analysis_path, 'fc_cs'
+    ))
+    plt.close('all')
+    
+    sc = sns.scatterplot(x='borrower_credit_score_at_origination',
+         y='original_combined_ltv', hue='is_foreclosed', 
+         data=acquisition_data[[
+             'borrower_credit_score_at_origination', 'original_combined_ltv',
+             'is_foreclosed'
+         ]]
+    )
+    sc.get_figure().savefig(
+        os.path.join(analysis_path, 'credit_vs_combined_ltv.png')
+    )
+    plt.close('all')
+    
+    pair_data = acquisition_data[[
+        'original_interest_rate', 'original_upb', 'original_loan_term',
+        'original_ltv', 'original_combined_ltv', 'original_dti',
+        'borrower_credit_score_at_origination', 'is_foreclosed',
+        'no_0_past_due_months', 'no_1_past_due_months'
+    ]].dropna().sample(10000)
+    pair = sns.pairplot(pair_data, hue='is_foreclosed')
+    pair.savefig(
+        os.path.join(analysis_path, 'pairs')        
+    )
+    
+    
+    
+    
+    
+    
+    
+    
