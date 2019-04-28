96fd8e4c3e649ce6923ee678606b5da849e703af
David Gillis <dgillis91@gmail.com>
2019-04-28 13:34:18 -0500

    standardized formatting

--- source/loandata.py
+++ source/loandata.py
@@ -5,8 +5,10 @@ Created on Fri Mar 15 20:15:45 2019
 @author: dgill
 """
 
+
 class Loan:
     __payment_status = set([str(x) for x in range(4)])
+    
     def __init__(self, loan_identifier):
         self._loan_identifier = loan_identifier
         self._payment_status_count = self._default_status_count()
@@ -15,9 +17,11 @@ class Loan:
     @property
     def loan_identifier(self):
         return self._loan_identifier
+
     @property
     def is_foreclosed(self):
         return self._is_foreclosed
+
     @is_foreclosed.setter
     def is_foreclosed(self, value):
         self._is_foreclosed = value
@@ -41,9 +45,12 @@ class Loan:
     
     def __ne__(self, other):
         return not self.__eq__(other)
+
     def __eq__(self, other):
         return self.__cmp__(other)
+
     def __cmp__(self, other):
         return self._loan_identifier == other._loan_identifier
+
     def __hash__(self, other):
         return hash(self._loan_identifier)
