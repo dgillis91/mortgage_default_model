c05ba49bc7ee3de13b8f77a70400dfa1472daeda
David Gillis <dgillis91@gmail.com>
2019-05-06 18:35:56 -0500

    initial commit

--- source/knn_model.py
+++ source/knn_model.py
@@ -7,15 +7,13 @@ Created on Sun Mar 17 19:41:06 2019
 
 import os
 
-from keras.models import Model
-from keras.layers import Dense, BatchNormalization, Input
-
 from sklearn.model_selection import train_test_split
+
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import SMOTE
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, LabelEncoder
 
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.metrics import classification_report
 
@@ -24,6 +22,8 @@ import pandas as pd
 
 from dirutil import project_directory
 from configfile import get_config
+
+from collections import defaultdict
 
 
 def clean_nulls(df, cols):
@@ -76,9 +76,6 @@ if __name__ == '__main__':
     
     # Pull out the predictors & target
     model_data = performance_data[predictors + [target]]
-    # del performance_data
-    
-    # Apply Transforms
 
     # Clean nulls and map f/c stat to bits
     clean_nulls(model_data, predictors)
@@ -87,43 +84,38 @@ if __name__ == '__main__':
     
     # split train and test data
     model_data = model_data.values.astype(np.float32)
-    # Scale predictors.
-    # scaler = MinMaxScaler(feature_range=(0, 1))
-    # model_data[:, 0:3] = scaler.fit_transform(model_data[:, 0:3])
-    
-    # Pull out 90% for training. Ensure data is shuffled.
-    full_train, full_test = train_test_split(model_data, shuffle=True, test_size=.1)
-    
-    predictor_train = full_train[:, 0:4]
-    predictor_test = full_test[:, 0:4]
-    target_train = full_train[:, 4]
-    target_test = full_test[:, 4]
 
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    predictor_test = scaler.fit_transform(predictor_test)
-    predictor_train = scaler.fit_transform(predictor_train)
-    
-    sampler = SamplerFactory.get_instance(sample_method, ratio=.5)
-    res_predictor_train, res_target_train = sampler.fit_sample(
-        predictor_train, target_train
-    )
-    
-    inputs = Input(shape=(4,))
-    x = Dense(64, activation='relu')(inputs)
-    x = Dense(64, activation='relu')(x)
-    predictions = Dense(1, activation='sigmoid')(x)
-    model = Model(inputs=inputs, outputs=predictions)
-    
-    model.compile(
-        loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy']
-    )
-    history = model.fit(
-        res_predictor_train, res_target_train, 
-        epochs=32, verbose=1, batch_size=64,
-        validation_data=(predictor_test, target_test)
-    )
+    knn_models = defaultdict(dict)
+    outfile_path = os.path.join(project_path, 'analysis', 'knn_analysis.csv')
+    with open(outfile_path, 'w') as grid_search_out:
+        grid_search_out.write('ratio,neighbor_count,precision_non_default,' +
+                              'recall_non_default,precision_default,recall_default\n')
+        sampling_ratios = np.linspace(.1, 1, 10)
+        for ratio in sampling_ratios:
+            full_train, full_test = train_test_split(model_data, shuffle=True, test_size=.1)
 
-    p = model.predict(predictor_test)
-    p_nominal = [1 if x > .5 else 0 for x in p]
+            predictor_train = full_train[:, 0:4]
+            predictor_test = full_test[:, 0:4]
+            target_train = full_train[:, 4]
+            target_test = full_test[:, 4]
 
-    print(classification_report(target_test, p_nominal))
+            sampler = SamplerFactory.get_instance(sample_method, ratio=ratio if ratio < 1 else 1)
+            res_predictor_train, res_target_train = sampler.fit_sample(
+                predictor_train, target_train
+            )
+
+            for neighbor_count in range(3, 20):
+                classifier = KNeighborsClassifier(n_neighbors=neighbor_count)
+                classifier.fit(res_predictor_train, res_target_train)
+
+                p = classifier.predict(predictor_test)
+
+                report = classification_report(target_test, p, output_dict=True)
+                results = '{},{},{},{},{},{}\n'.format(
+                    ratio, neighbor_count,
+                    report['0.0']['precision'], report['0.0']['recall'],
+                    report['1.0']['precision'], report['1.0']['recall']
+                )
+                print(ratio)
+                grid_search_out.write(results)
+                knn_models[ratio][neighbor_count] = classifier
